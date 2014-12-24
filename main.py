4905b7dd4ec6608c1733fcd0c5a071aa359a793f
Christian Svensson <blue@cmd.nu>
2014-12-24 18:46:18 +0100

    WIP for Node.js stuff

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import json
 import uuid
 import webapp2
 
-from google.appengine.api import channel
+from google.appengine.api import urlfetch
+
+
+#RTCBRIDGE_ENDPOINT = 'https://rtcbridge-dot-bluecmd0.appspot.com/connect'
+RTCBRIDGE_ENDPOINT = 'http://bob.cmd.nu:8000/connect'
 
 
 class IndexPage(webapp2.RequestHandler):
@@ -11,15 +15,22 @@ class IndexPage(webapp2.RequestHandler):
     self.redirect('/fastgame.html', permanent=True)
 
 
-class ChannelPage(webapp2.RequestHandler):
+class ConnectPage(webapp2.RequestHandler):
+  """Connect a client with the RTC bridge backend."""
 
-  def get(self):
-    token = channel.create_channel(str(uuid.uuid1()))
+  def post(self):
+    # Filter incoming offer
+    offer = json.loads(self.request.body)
+    payload = json.dumps({'sdp': offer['sdp'], 'type': offer['type']})
+    result = urlfetch.fetch(
+        url=RTCBRIDGE_ENDPOINT, payload=payload, method=urlfetch.POST,
+        headers={'Content-Type': 'application/json'}, follow_redirects=False)
+
     self.response.headers['Content-Type'] = 'application/json'
-    self.response.write(json.dumps({'token': token}))
+    self.response.write(result.content)
 
 
 application = webapp2.WSGIApplication([
   ('/', IndexPage),
-  ('/channel', ChannelPage),
+  ('/connect', ConnectPage),
   ], debug=True)
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var appengine = require('appengine');
+var parser = require('body-parser');
 var express = require('express');
+var webrtc = require('wrtc');
 
 var app = express();
 
 app.use(appengine.middleware.base);
+app.use(parser.json());
 
 app.get('/_ah/health', function(req, res) {
   res.set('Content-Type', 'text/plain');
@@ -21,9 +24,37 @@ app.get('/_ah/stop', function(req, res) {
   process.exit();
 });
 
-app.get('/hello', function(req, res) {
-  var env = req.appengine.devappserver ? 'the dev appserver' : 'production';
-  res.send('Hello, world from ' + env + '!');
+app.post('/connect', function(req, res) {
+  appengine.logOneLine(req, 'New connection', appengine.LogLevel.INFO);
+
+  var mediaConstraints = {
+    'optional': [{
+      'RtpDataChannels': true,
+      'DtlsSrtpKeyAgreement': false
+    }]
+  };
+
+  var pc = new webrtc.RTCPeerConnection(
+    {iceServers: [{url:'stun:stun.l.google.com:19302'}]},
+    mediaConstraints);
+
+  pc.onicecandidate = function(candidate) {
+    var msg = JSON.stringify(candidate);
+    appengine.logOneLine(req, 'New candidate: ' + msg, appengine.LogLevel.INFO);
+  }
+
+  var offer = new webrtc.RTCSessionDescription(
+    {'sdp': req.body.sdp, 'type': req.body.type});
+
+  pc.setRemoteDescription(
+      offer, function() {
+    pc.createAnswer(function(answer) {
+      pc.setLocalDescription(answer, function() {
+        res.json({'sdp': answer.sdp, 'type': answer.type});
+      });
+    });
+  });
+  res.json({});
 });
 
 app.listen(8080, '0.0.0.0');
